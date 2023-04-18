df68fa60435739f61db02fe09a34117ca6a6af5f
v_hllhliang <v_hllhliang@tencent.com>
2023-04-18 15:04:02 +0800

    Squashed commit of the following:
    commit 5836b9bece24480fe581cf61ff9d98ea5f9fac97
    Author: v_hllhliang <v_hllhliang@tencent.com>
    Date:   Tue Apr 18 14:55:23 2023 +0800
    
        https://geektutu.com/post/geerpc-day1.html

---
+++ client.go
@@ -0,0 +1,216 @@
+package geerpc
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"net"
+	"sync"
+
+	"geerpc/codec"
+)
+
+type Call struct {
+	Seq           uint64
+	ServiceMethod string
+	Args          interface{}
+	Reply         interface{}
+	Error         error
+	Done          chan *Call
+}
+
+func (c *Call) done() {
+	c.Done <- c
+}
+
+type Client struct {
+	cc       codec.Codec
+	otp      *Option
+	sending  sync.Mutex
+	header   codec.Header
+	mu       sync.Mutex
+	sqe      uint64
+	pending  map[uint64]*Call
+	closing  bool
+	shutdown bool
+}
+
+var ErrShutdown = errors.New("connection is shut down")
+
+func (c *Client) Close() error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if c.closing {
+		return ErrShutdown
+	}
+	c.closing = true
+	return c.cc.Close()
+}
+
+func (c *Client) IsAvailable() bool {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	return !c.shutdown && !c.closing
+}
+
+func (c *Client) registerCall(call *Call) (uint64, error) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if c.closing || c.shutdown {
+		return 0, ErrShutdown
+	}
+	call.Seq = c.sqe
+	c.pending[call.Seq] = call
+	c.sqe++
+	return call.Seq, nil
+}
+
+func (c *Client) removeCall(seq uint64) *Call {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	call := c.pending[seq]
+	delete(c.pending, seq)
+	return call
+}
+
+func (c *Client) terminateCalls(err error) {
+	c.sending.Lock()
+	defer c.sending.Unlock()
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.shutdown = true
+	for _, call := range c.pending {
+		call.Error = err
+		call.done()
+	}
+}
+
+func (c *Client) receive() {
+	var err error
+	for err == nil {
+		var h codec.Header
+		if err = c.cc.ReadHeader(&h); err != nil {
+			break
+		}
+		call := c.removeCall(h.Seq)
+		switch {
+		case call == nil:
+			err = c.cc.ReadBody(nil)
+		case h.Error != "":
+			call.Error = fmt.Errorf(h.Error)
+			err = c.cc.ReadBody(nil)
+			call.done()
+		default:
+			err = c.cc.ReadBody(call.Reply)
+			if err != nil {
+				call.Error = errors.New("reading body " + err.Error())
+			}
+			call.done()
+		}
+	}
+	c.terminateCalls(err)
+}
+
+func NewClient(conn net.Conn, opt *Option) (*Client, error) {
+	f := codec.NewCodecFuncMap[opt.CodecType]
+	if f == nil {
+		err := fmt.Errorf("invalid codec type %s", opt.CodecType)
+		log.Println("rpc client:codec error:", err)
+		return nil, err
+	}
+	if err := json.NewEncoder(conn).Encode(&opt); err != nil {
+		log.Println("rpc client: codec error:", err)
+		conn.Close()
+		return nil, err
+	}
+	return NewClientCodec(f(conn), opt), nil
+}
+
+func NewClientCodec(cc codec.Codec, opt *Option) *Client {
+	client := &Client{
+		cc:      cc,
+		otp:     opt,
+		sqe:     1,
+		pending: make(map[uint64]*Call),
+	}
+	go client.receive()
+	return client
+}
+
+func Dial(network, address string, opts ...*Option) (*Client, error) {
+	opt, err := parseOptions(opts...)
+	if err != nil {
+		return nil, err
+	}
+	conn, err := net.Dial(network, address)
+	if err != nil {
+		return nil, err
+	}
+	client, err := NewClient(conn, opt)
+	if err != nil {
+		conn.Close()
+		return nil, err
+	}
+	return client, nil
+}
+
+func parseOptions(opts ...*Option) (*Option, error) {
+	if len(opts) == 0 || opts[0] == nil {
+		return DefaultOption, nil
+	}
+	if len(opts) != 1 {
+		return nil, errors.New("number of options is more than 1")
+	}
+	opt := opts[0]
+	opt.MagicNumber = DefaultOption.MagicNumber
+	if opt.CodecType == "" {
+		opt.CodecType = DefaultOption.CodecType
+	}
+	return opt, nil
+}
+
+func (c *Client) send(call *Call) {
+	c.sending.Lock()
+	defer c.sending.Unlock()
+
+	seq, err := c.registerCall(call)
+	if err != nil {
+		call.Error = err
+		call.done()
+		return
+	}
+
+	c.header.ServiceMethod = call.ServiceMethod
+	c.header.Seq = call.Seq
+	c.header.Error = ""
+
+	if err := c.cc.Write(&c.header, call.Args); err != nil {
+		call := c.removeCall(seq)
+		if call != nil {
+			call.Error = err
+			call.done()
+		}
+	}
+}
+
+func (c *Client) Go(serviceMethod string, args, reply interface{}, done chan *Call) *Call {
+	if done == nil {
+		done = make(chan *Call, 0)
+	} else if cap(done) == 0 {
+		log.Panic("rpc client: done channel is unbuffered")
+	}
+	call := &Call{
+		ServiceMethod: serviceMethod,
+		Args:          args,
+		Reply:         reply,
+		Done:          done,
+	}
+	c.send(call)
+	return call
+}
+
+func (c *Client) Call(serviceMethod string, args, reply interface{}) error {
+	call := <-c.Go(serviceMethod, args, reply, make(chan *Call, 1)).Done
+	return call.Error
+}
